ee0fc4b8ff2d2d46f35757595ad1efbef742bc27
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-11-16 18:06:27 +0100

    feat(routes) base of routes managment

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"public-api/src"
+	"time"
 )
 
 func main() {
 	r := mux.NewRouter()
-	/*r.HandleFunc("/", HomeHandler)*/
-	http.Handle("/", r)
+	r.HandleFunc("/", src.HomeHandler)
+	srv := &http.Server{
+		Handler: r,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
 }
---
+++ src/handler.go
@@ -0,0 +1,25 @@
+package src
+
+import (
+	"encoding/json"
+	"net/http"
+	"public-api/src/response"
+)
+
+func defaultHandler(r *http.Request) {
+	r.Header.Set("Content-Type", "application/json")
+}
+
+func writeResponse(w http.ResponseWriter, resp *response.Response) error {
+	w.WriteHeader(resp.StatusCode)
+	return json.NewEncoder(w).Encode(resp)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	defaultHandler(r)
+	resp := response.Response{StatusCode: 200, Message: "Welcome to the API"}
+	err := writeResponse(w, &resp)
+	if err != nil {
+		panic(err)
+	}
+}
